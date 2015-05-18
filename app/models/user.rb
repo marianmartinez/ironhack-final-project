22a6499216914e4d278978be7c624f2c6167a2b6
Marian Martínez <marian.mtnz@gmail.com>
2015-05-19 01:59:47 +0200

    Pass test to validate password min-length (green)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   # look for gem:validates_email_format_of :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
-  validates :password, confirmation: true
+  validates :password, confirmation: true, length: { minimum: 3 }
   attr_accessor :password, :password_confirmation
 end
