962cb2abe261bd5a76709791e56c001c3113428d
Marian Martínez <marian.mtnz@gmail.com>
2015-05-18 17:17:32 +0200

    Add name to users table and migrate

--- db/migrate/20150518150600_sorcery_core.rb
+++ db/migrate/20150518150600_sorcery_core.rb
@@ -1,6 +1,7 @@
 class SorceryCore < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
       t.string :email,            :null => false
       t.string :crypted_password
       t.string :salt
