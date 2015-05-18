9fb42f3e5d96aa6cb066ddb71e7e36279a30a89a
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-05-18 22:41:53 +0200

    minimal working test config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :name, presence: true, on: :update
+
+  validates :email, presence: true, uniqueness: true
+
+  # look for gem:validates_email_format_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
+  validates :password, confirmation: true
+  attr_accessor :password, :password_confirmation
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,16 @@
 
+require 'rails_helper'
+
+describe User do
+  it 'has a valid factory' do
+    expect(build(:user)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = build(:user, name: nil)
+
+    expect(user).to_not be_valid
+  end
+
+
+end
