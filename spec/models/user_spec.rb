1f1cc7f2638899cce16e09096666e4606c53d69f
Marian Martínez <marian.mtnz@gmail.com>
2015-05-19 01:36:22 +0200

    Refactor email presence test with shoulda matchers sintax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,11 @@
-
 require 'rails_helper'
 
 describe User do
+
   it 'has a valid factory' do
     expect(build(:user)).to be_valid
   end
 
-  it 'is invalid without an email' do
-    email = build(:user, email: nil)
-
-    expect(email).to_not be_valid
-  end
-
+  it { should validate_presence_of(:email) }
+  
 end
