5a55fcd81af03b4cbd32aaaa5c8da934fcdd67b7
Marian Martínez <marian.mtnz@gmail.com>
2015-05-19 14:56:33 +0200

    Redirect a user to the page they were trying to see

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     if login(params[:email], params[:password])
       flash[:success] = 'Welcome back!'
-      redirect_to root_path
+      redirect_back_or_to root_path
     else
       flash.now[:warning] = 'E-mail and/or password is incorrect.'
       render 'new'
