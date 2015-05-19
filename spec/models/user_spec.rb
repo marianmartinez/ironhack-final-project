c95c947f6a4230ebbc24e1877f8354d59459d51f
Marian Martínez <marian.mtnz@gmail.com>
2015-05-19 02:09:02 +0200

    Add confirmation of password test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,6 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should ensure_length_of(:password).is_at_least(3) }
+  it { should validate_confirmation_of(:password) }
 
 end
