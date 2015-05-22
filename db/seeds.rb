c9303398b1e70fe1a267b9dc1a0f8827d87a3ed5
Marian Martínez <marian.mtnz@gmail.com>
2015-05-22 11:07:04 +0200

    Seed the DB with fake users and Tracks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+t= Track.create name: "Web development"
+t2= Track.create name: "UX"
+t3= Track.create name: "Web Design"
+t4= Track.create name: "Marketing"
+
+10.times do
+  User.create(name: FFaker::Name.name, email: FFaker::Internet.email, password: 123)
+end
+
+u = User.create(name: 'Marian Martínez', email: 'marian.mtnz@gmail.com', password: 123)
+u = User.create(name: 'Pepe López', email: 'pepe@pepe.com', password: 123)
