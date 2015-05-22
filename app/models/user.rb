e5045fec8fd4a04dc715f4289fb7c898148ac914
Marian Martínez <marian.mtnz@gmail.com>
2015-05-22 15:06:13 +0200

    validate picture size

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,31 @@
 class User < ActiveRecord::Base
   has_many :tracks
+
   authenticates_with_sorcery!
+
   mount_uploader :picture, PictureUploader
 
   validates :name, presence: true, on: :update
-
   validates :email, presence: true, uniqueness: true,
              email_format: { message: 'has invalid format' }
-
   validates :password, confirmation: true, length: { minimum: 3 }
+  validate  :picture_size
+
   attr_accessor :password, :password_confirmation
+
   def admin?
     email == 'marian.mtnz@gmail.com'
   end
+
   def self.admin
     find_by email: 'marian.mtnz@gmail.com'
+  end
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
   end
 end
