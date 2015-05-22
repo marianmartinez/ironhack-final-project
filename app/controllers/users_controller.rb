2b5877d8357b1a0310747152f7f76b1cf9575d1e
Marian Martínez <marian.mtnz@gmail.com>
2015-05-22 14:57:28 +0200

    Add picture to the list of attributes permitted to be modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :picture)
   end
 end
