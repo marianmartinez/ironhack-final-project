85b5a5035eb11b319d587e94e6e2d6a11d63aada
Marian Martínez <marian.mtnz@gmail.com>
2015-05-22 09:29:33 +0200

    Add Tracks validation

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,7 @@
 class Track < ActiveRecord::Base
   belongs_to :user
+  validates :name, uniqueness: true,
+                     presence: true,
+                     length: {maximum: 30},
+                     format: {with: /\A[a-zA-Z0-9\s]+\z/ }
 end
