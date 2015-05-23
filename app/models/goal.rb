61fa057526d55c05d695e93ab53cffbd476b5d15
Marian Martínez <marian.mtnz@gmail.com>
2015-05-23 20:33:10 +0200

    fix errors

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,12 +1,12 @@
 class Goal < ActiveRecord::Base
   belongs_to :track
-  has_many :sources dependent: :destroy
+  has_many :sources, dependent: :destroy
   validates :name, presence: true,
                    length: {maximum: 30},
                    format: {with: /\A[a-zA-Z0-9\s]+\z/ }
   validates :description, presence: true,
                    length: {maximum: 250},
                    format: {with: /\A[a-zA-Z0-9\s]+\z/ }
-  vallidates :goal_date, presence: true
+  validates :goal_date, presence: true
   validates :accomplished, presence: true, numericality: true
 end
