1eb624237fb8c458617b80360241d75be8cf9015
Marian Martínez <marian.mtnz@gmail.com>
2015-05-23 17:56:23 +0200

    add scope to Track uniqueness validation

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :user
-  validates :name, uniqueness: true,
+  validates :name, uniqueness: { scope: :user_id },
                      presence: true,
                      length: {maximum: 30},
                      format: {with: /\A[a-zA-Z0-9\s]+\z/ }
