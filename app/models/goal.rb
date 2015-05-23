5f05a2a04477db5f4af031a26c9145123be912f4
Marian Martínez <marian.mtnz@gmail.com>
2015-05-23 19:35:55 +0200

    create Source model and Goal-Source association

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,6 @@
 class Goal < ActiveRecord::Base
   belongs_to :track
+  has_many :sources
   validates :name, presence: true,
                    length: {maximum: 30},
                    format: {with: /\A[a-zA-Z0-9\s]+\z/ }
