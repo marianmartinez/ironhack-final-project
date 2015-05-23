26958f4b1abc22e609489c157481ff081b5a0036
Marian Martínez <marian.mtnz@gmail.com>
2015-05-23 18:03:03 +0200

    Add Track-Goal relationship

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :user
+  has_many :goals
   validates :name, uniqueness: { scope: :user_id },
                      presence: true,
                      length: {maximum: 30},
