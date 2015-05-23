3d89d98634284de6b214eca27a9fd249ba4d7461
Marian Martínez <marian.mtnz@gmail.com>
2015-05-23 13:05:59 +0200

    try to make a simple search in google books

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,18 @@ class PagesController < ApplicationController
   def home
 
   end
+
+  def search
+    if params[:search]
+      @resources = GoogleBooks.search(
+          "#{params[:search]}",
+          {
+           :count => 10, :page => 1,
+           :api_key => 'AIzaSyCGZFL2tOiMcg3G5V4KC6ViLQcnrt29Jo4'
+          }
+        )
+    else
+      @resources = []
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :tracks
   root to: 'pages#index'
-  get "help"  => "static_pages#help",           :as => "help"
+  get "help"  => "static_pages#help",     :as => "help"
+  get "search"  => "pages#search",        :as => "search_page"
+  post "search"  => "pages#search",       :as => "search"
   get "login"   => "sessions#new",        :as => "login"
   get "logout"  => "sessions#destroy",    :as => "logout"
   get "signup"  => "users#new",           :as => "signup"
