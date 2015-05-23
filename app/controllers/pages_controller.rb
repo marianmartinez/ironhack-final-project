70b02ace479d6a855a861c060e690c9958c34ed2
Marian Martínez <marian.mtnz@gmail.com>
2015-05-23 14:41:59 +0200

    try to count items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
 
   def search
     if params[:search]
+      @query_word = params[:search]
       @resources = GoogleBooks.search(
           "#{params[:search]}",
           {
